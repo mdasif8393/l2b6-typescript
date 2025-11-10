15ba22b89570f9eb23f77cad9caafc50bf15216b
mdasif8393 <md.asif.8393@gmail.com>
2025-11-10 11:52:17 +0600

    function ts

---
+++ src/function.ts
@@ -0,0 +1,24 @@
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+add(2, 3);
+
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+// object => function => method
+
+const poorUser = {
+  name: "mezba",
+  balance: 0,
+  addBalance(value: number): number {
+    const totalBalance = this.balance + value;
+    return totalBalance;
+  },
+};
+
+poorUser.addBalance(1000);
+
+const arr: number[] = [1, 4, 6];
+
+const srqArray = arr.map((x: number): number => x * x);
