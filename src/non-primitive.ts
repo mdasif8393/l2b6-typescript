5a56aadf8451025974895958906e330aa6fc9044
mdasif8393 <md.asif.8393@gmail.com>
2025-11-10 10:49:50 +0600

    non primitive data type

---
+++ src/non-primitive.ts
@@ -0,0 +1,10 @@
+// array
+let friends: string[] = ["egg", "potato", "milk"];
+friends.push(12);
+
+let mixesArr: (string | number)[] = ["egg", 12, "milk", 1, "sugar", 2];
+
+// tuple
+let coordinated: [number, number] = [23, 34];
+
+let nameRoll: [string, number] = ["Asif", 38];
