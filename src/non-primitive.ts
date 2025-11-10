f8ea59abe7e4d55c4e84da852ba6ef4fcb825ada
mdasif8393 <md.asif.8393@gmail.com>
2025-11-10 11:28:25 +0600

    non primitive data type

--- src/non-primitive.ts
+++ src/non-primitive.ts
@@ -1,6 +1,6 @@
 // array
 let friends: string[] = ["egg", "potato", "milk"];
-friends.push(12);
+// friends.push(12);
 
 let mixesArr: (string | number)[] = ["egg", 12, "milk", 1, "sugar", 2];
 
@@ -8,3 +8,16 @@ let mixesArr: (string | number)[] = ["egg", 12, "milk", 1, "sugar", 2];
 let coordinated: [number, number] = [23, 34];
 
 let nameRoll: [string, number] = ["Asif", 38];
+
+const user: {
+  organization: "programmingHero";
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "programmingHero",
+  firstName: "Mezba",
+  lastName: "Forhan",
+  isMarried: false,
+};
